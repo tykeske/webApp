7e77e1df28911d4465375830b2fd6cd1483c7d4f
BeccaMenefee <becca.menefeescheets@pcc.edu>
2020-04-18 13:53:36 -0700

    Added controls to form

--- FINS/SendNotification/Form1.Designer.cs
+++ FINS/SendNotification/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(94, 73);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(164, 13);
+            this.messageLabel.TabIndex = 0;
+            this.messageLabel.Text = "Please enter message to be sent:";
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(97, 108);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(436, 234);
+            this.messageTextBox.TabIndex = 1;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(278, 390);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(630, 450);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.messageLabel);
+            this.Name = "Form1";
+            this.Text = "Send Notification";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button submitButton;
     }
 }
 
