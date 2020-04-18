a43fcbd404fe5c2fa64bfdaa1a89b57b733eb4b3
vince-amela <vincent.amela@pcc.edu>
2020-04-18 15:17:25 -0700

    ui change

--- FINS/TemplateCreation/Form1.Designer.cs
+++ FINS/TemplateCreation/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tempLabel = new System.Windows.Forms.Label();
+            this.tempTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tempLabel
+            // 
+            this.tempLabel.AutoSize = true;
+            this.tempLabel.Location = new System.Drawing.Point(33, 30);
+            this.tempLabel.Name = "tempLabel";
+            this.tempLabel.Size = new System.Drawing.Size(85, 13);
+            this.tempLabel.TabIndex = 0;
+            this.tempLabel.Text = "Template Name:";
+            // 
+            // tempTextBox
+            // 
+            this.tempTextBox.Location = new System.Drawing.Point(141, 23);
+            this.tempTextBox.Name = "tempTextBox";
+            this.tempTextBox.Size = new System.Drawing.Size(100, 20);
+            this.tempTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tempTextBox);
+            this.Controls.Add(this.tempLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label tempLabel;
+        private System.Windows.Forms.TextBox tempTextBox;
     }
 }
 
