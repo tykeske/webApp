b44104cbef04c2e4c3dcdc9d86efdc4a7ca692f1
vince-amela <vincent.amela@pcc.edu>
2020-04-19 15:27:49 -0700

    gui with code

--- FINS/TemplateCreation/templateCreationForm.Designer.cs
+++ FINS/TemplateCreation/templateCreationForm.Designer.cs
@@ -28,35 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tempLabel = new System.Windows.Forms.Label();
-            this.tempTextBox = new System.Windows.Forms.TextBox();
+            this.tempNameLabel = new System.Windows.Forms.Label();
+            this.tempNameTextBox = new System.Windows.Forms.TextBox();
+            this.tagTextBox3 = new System.Windows.Forms.TextBox();
+            this.tagTextBox1 = new System.Windows.Forms.TextBox();
+            this.tagTextBox2 = new System.Windows.Forms.TextBox();
+            this.msgBodyTextBox = new System.Windows.Forms.TextBox();
+            this.tagGroupBox = new System.Windows.Forms.GroupBox();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.insertTagButton1 = new System.Windows.Forms.Button();
+            this.insertTagButton3 = new System.Windows.Forms.Button();
+            this.deleteTagButton3 = new System.Windows.Forms.Button();
+            this.deleteTagButton2 = new System.Windows.Forms.Button();
+            this.insertTagButton2 = new System.Windows.Forms.Button();
+            this.deleteTagButton1 = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.tagGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tempLabel
+            // tempNameLabel
             // 
-            this.tempLabel.AutoSize = true;
-            this.tempLabel.Location = new System.Drawing.Point(36, 30);
-            this.tempLabel.Name = "tempLabel";
-            this.tempLabel.Size = new System.Drawing.Size(85, 13);
-            this.tempLabel.TabIndex = 0;
-            this.tempLabel.Text = "Template Name:";
+            this.tempNameLabel.AutoSize = true;
+            this.tempNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempNameLabel.Location = new System.Drawing.Point(31, 41);
+            this.tempNameLabel.Name = "tempNameLabel";
+            this.tempNameLabel.Size = new System.Drawing.Size(99, 15);
+            this.tempNameLabel.TabIndex = 0;
+            this.tempNameLabel.Text = "Template Name:";
             // 
-            // tempTextBox
+            // tempNameTextBox
             // 
-            this.tempTextBox.Location = new System.Drawing.Point(477, 30);
-            this.tempTextBox.Name = "tempTextBox";
-            this.tempTextBox.Size = new System.Drawing.Size(100, 20);
-            this.tempTextBox.TabIndex = 1;
+            this.tempNameTextBox.Location = new System.Drawing.Point(145, 41);
+            this.tempNameTextBox.Name = "tempNameTextBox";
+            this.tempNameTextBox.Size = new System.Drawing.Size(177, 20);
+            this.tempNameTextBox.TabIndex = 1;
+            // 
+            // tagTextBox3
+            // 
+            this.tagTextBox3.Location = new System.Drawing.Point(111, 115);
+            this.tagTextBox3.Name = "tagTextBox3";
+            this.tagTextBox3.Size = new System.Drawing.Size(114, 20);
+            this.tagTextBox3.TabIndex = 2;
+            // 
+            // tagTextBox1
+            // 
+            this.tagTextBox1.Enabled = false;
+            this.tagTextBox1.Location = new System.Drawing.Point(111, 35);
+            this.tagTextBox1.Name = "tagTextBox1";
+            this.tagTextBox1.Size = new System.Drawing.Size(114, 20);
+            this.tagTextBox1.TabIndex = 3;
+            this.tagTextBox1.Text = "Location";
+            // 
+            // tagTextBox2
+            // 
+            this.tagTextBox2.Enabled = false;
+            this.tagTextBox2.Location = new System.Drawing.Point(111, 79);
+            this.tagTextBox2.Name = "tagTextBox2";
+            this.tagTextBox2.Size = new System.Drawing.Size(114, 20);
+            this.tagTextBox2.TabIndex = 4;
+            this.tagTextBox2.Text = "FoodType";
+            // 
+            // msgBodyTextBox
+            // 
+            this.msgBodyTextBox.Location = new System.Drawing.Point(34, 336);
+            this.msgBodyTextBox.Multiline = true;
+            this.msgBodyTextBox.Name = "msgBodyTextBox";
+            this.msgBodyTextBox.Size = new System.Drawing.Size(571, 292);
+            this.msgBodyTextBox.TabIndex = 5;
+            // 
+            // tagGroupBox
+            // 
+            this.tagGroupBox.Controls.Add(this.deleteTagButton1);
+            this.tagGroupBox.Controls.Add(this.insertTagButton2);
+            this.tagGroupBox.Controls.Add(this.deleteTagButton2);
+            this.tagGroupBox.Controls.Add(this.deleteTagButton3);
+            this.tagGroupBox.Controls.Add(this.insertTagButton3);
+            this.tagGroupBox.Controls.Add(this.insertTagButton1);
+            this.tagGroupBox.Controls.Add(this.tagTextBox2);
+            this.tagGroupBox.Controls.Add(this.tagTextBox1);
+            this.tagGroupBox.Controls.Add(this.tagTextBox3);
+            this.tagGroupBox.Location = new System.Drawing.Point(34, 92);
+            this.tagGroupBox.Name = "tagGroupBox";
+            this.tagGroupBox.Size = new System.Drawing.Size(571, 167);
+            this.tagGroupBox.TabIndex = 6;
+            this.tagGroupBox.TabStop = false;
+            this.tagGroupBox.Text = "Tags";
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabel.Location = new System.Drawing.Point(31, 301);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(156, 15);
+            this.messageLabel.TabIndex = 7;
+            this.messageLabel.Text = "Please Type Your Message";
+            // 
+            // insertTagButton1
+            // 
+            this.insertTagButton1.Location = new System.Drawing.Point(284, 32);
+            this.insertTagButton1.Name = "insertTagButton1";
+            this.insertTagButton1.Size = new System.Drawing.Size(75, 23);
+            this.insertTagButton1.TabIndex = 5;
+            this.insertTagButton1.Text = "Insert Tag";
+            this.insertTagButton1.UseVisualStyleBackColor = true;
+            this.insertTagButton1.Click += new System.EventHandler(this.insertTagButton1_Click);
+            // 
+            // insertTagButton3
+            // 
+            this.insertTagButton3.Location = new System.Drawing.Point(284, 112);
+            this.insertTagButton3.Name = "insertTagButton3";
+            this.insertTagButton3.Size = new System.Drawing.Size(75, 23);
+            this.insertTagButton3.TabIndex = 6;
+            this.insertTagButton3.Text = "Insert Tag";
+            this.insertTagButton3.UseVisualStyleBackColor = true;
+            this.insertTagButton3.Click += new System.EventHandler(this.insertTagButton3_Click);
+            // 
+            // deleteTagButton3
+            // 
+            this.deleteTagButton3.Location = new System.Drawing.Point(406, 112);
+            this.deleteTagButton3.Name = "deleteTagButton3";
+            this.deleteTagButton3.Size = new System.Drawing.Size(75, 23);
+            this.deleteTagButton3.TabIndex = 7;
+            this.deleteTagButton3.Text = "Delete Tag";
+            this.deleteTagButton3.UseVisualStyleBackColor = true;
+            // 
+            // deleteTagButton2
+            // 
+            this.deleteTagButton2.Location = new System.Drawing.Point(406, 76);
+            this.deleteTagButton2.Name = "deleteTagButton2";
+            this.deleteTagButton2.Size = new System.Drawing.Size(75, 23);
+            this.deleteTagButton2.TabIndex = 8;
+            this.deleteTagButton2.Text = "Delete Tag";
+            this.deleteTagButton2.UseVisualStyleBackColor = true;
+            // 
+            // insertTagButton2
+            // 
+            this.insertTagButton2.Location = new System.Drawing.Point(284, 76);
+            this.insertTagButton2.Name = "insertTagButton2";
+            this.insertTagButton2.Size = new System.Drawing.Size(75, 23);
+            this.insertTagButton2.TabIndex = 9;
+            this.insertTagButton2.Text = "Insert Tag";
+            this.insertTagButton2.UseVisualStyleBackColor = true;
+            this.insertTagButton2.Click += new System.EventHandler(this.insertTagButton2_Click);
+            // 
+            // deleteTagButton1
+            // 
+            this.deleteTagButton1.Location = new System.Drawing.Point(406, 32);
+            this.deleteTagButton1.Name = "deleteTagButton1";
+            this.deleteTagButton1.Size = new System.Drawing.Size(75, 23);
+            this.deleteTagButton1.TabIndex = 10;
+            this.deleteTagButton1.Text = "Delete Tag";
+            this.deleteTagButton1.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(247, 655);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "&Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // templateCreationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tempTextBox);
-            this.Controls.Add(this.tempLabel);
+            this.ClientSize = new System.Drawing.Size(667, 703);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.tagGroupBox);
+            this.Controls.Add(this.msgBodyTextBox);
+            this.Controls.Add(this.tempNameTextBox);
+            this.Controls.Add(this.tempNameLabel);
             this.Name = "templateCreationForm";
-            this.Text = "Form1";
+            this.Text = "Template Creation";
+            this.tagGroupBox.ResumeLayout(false);
+            this.tagGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -64,8 +213,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Label tempLabel;
-        private System.Windows.Forms.TextBox tempTextBox;
+        private System.Windows.Forms.Label tempNameLabel;
+        private System.Windows.Forms.TextBox tempNameTextBox;
+        private System.Windows.Forms.TextBox tagTextBox3;
+        private System.Windows.Forms.TextBox tagTextBox1;
+        private System.Windows.Forms.TextBox tagTextBox2;
+        private System.Windows.Forms.TextBox msgBodyTextBox;
+        private System.Windows.Forms.GroupBox tagGroupBox;
+        private System.Windows.Forms.Button deleteTagButton1;
+        private System.Windows.Forms.Button insertTagButton2;
+        private System.Windows.Forms.Button deleteTagButton2;
+        private System.Windows.Forms.Button deleteTagButton3;
+        private System.Windows.Forms.Button insertTagButton3;
+        private System.Windows.Forms.Button insertTagButton1;
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Button saveButton;
     }
 }
 
--- FINS/TemplateCreation/templateCreationForm.cs
+++ FINS/TemplateCreation/templateCreationForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TemplateCreation
 {
@@ -15,6 +16,76 @@ namespace TemplateCreation
         public templateCreationForm()
         {
             InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (tempNameTextBox.TextLength < 51 && tempNameTextBox.TextLength > 0 && msgBodyTextBox.TextLength < 1001)
+            {
+                createTemplate();
+            }
+            else
+            {
+                MessageBox.Show("You must enter a template name, 1-50 characters.  Msg body field has 1000 character limit.");
+            }
+
+        }
+        private void createTemplate()
+        {
+            try
+            {
+                string templateName = tempNameTextBox.Text;
+                string msgContent = msgBodyTextBox.Text;
+                string createDate = DateTime.Now.ToString();
+                string upDated = DateTime.Now.ToString();
+                int createdBy = 1;
+                int updatedBy = 1;
+
+                using (SqlConnection connection = new SqlConnection("Data Source=cisdbss.pcc.edu; Initial Catalog=234a_TeamApex; User id=234a_TeamApex; Password=^&%_2020_Spring_TeamApex"))
+                {
+                    String query = "INSERT INTO dbo.message_template (template_name, message_content, created_date, updated_date, created_by, updated_by) VALUES (@templateName,@msgContent,@created_date, @updated_date, @created_by, @updated_by)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.Add("@templateName", SqlDbType.NVarChar, 50).Value = templateName;
+                        command.Parameters.Add("@msgContent", SqlDbType.NVarChar, 1000).Value = msgContent;
+                        command.Parameters.Add("@created_date", SqlDbType.SmallDateTime, 19).Value = createDate;
+                        command.Parameters.Add("@updated_date", SqlDbType.SmallDateTime, 19).Value = upDated;
+                        command.Parameters.Add("@created_by", SqlDbType.Int, 50).Value = createdBy;
+                        command.Parameters.Add("@updated_by", SqlDbType.Int, 50).Value = updatedBy;
+                        connection.Open();
+                        int result = command.ExecuteNonQuery();
+
+                        // Check Error
+                        if (result < 0)
+                            Console.WriteLine("Error inserting data into Database!");
+                    }
+                }
+                MessageBox.Show("Template Created!");
+
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong.");
+            }
+        }
+
+        private void insertTagButton1_Click(object sender, EventArgs e)
+        {
+            string tagValue = "<" + tagTextBox1.Text + ">";
+            msgBodyTextBox.AppendText(tagValue);
+        }
+
+        private void insertTagButton2_Click(object sender, EventArgs e)
+        {
+            string tagValue = "<" + tagTextBox2.Text + ">";
+            msgBodyTextBox.AppendText(tagValue);
+        }
+
+        private void insertTagButton3_Click(object sender, EventArgs e)
+        {
+            string tagValue = "<" + tagTextBox3.Text + ">";
+            msgBodyTextBox.AppendText(tagValue);
         }
     }
 }
