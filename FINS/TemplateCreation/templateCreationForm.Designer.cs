9f1c1640bfa2c606798102f5ebbb8cd7eb651245
vince-amela <vincent.amela@pcc.edu>
2020-04-19 15:05:20 -0700

    rename and general form creation

--- FINS/TemplateCreation/templateCreationForm.Designer.cs
+++ FINS/TemplateCreation/templateCreationForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TemplateCreation
 {
-    partial class Form1
+    partial class templateCreationForm
     {
         /// <summary>
         /// Required designer variable.
@@ -35,7 +35,7 @@
             // tempLabel
             // 
             this.tempLabel.AutoSize = true;
-            this.tempLabel.Location = new System.Drawing.Point(33, 30);
+            this.tempLabel.Location = new System.Drawing.Point(36, 30);
             this.tempLabel.Name = "tempLabel";
             this.tempLabel.Size = new System.Drawing.Size(85, 13);
             this.tempLabel.TabIndex = 0;
@@ -43,19 +43,19 @@
             // 
             // tempTextBox
             // 
-            this.tempTextBox.Location = new System.Drawing.Point(141, 23);
+            this.tempTextBox.Location = new System.Drawing.Point(477, 30);
             this.tempTextBox.Name = "tempTextBox";
             this.tempTextBox.Size = new System.Drawing.Size(100, 20);
             this.tempTextBox.TabIndex = 1;
             // 
-            // Form1
+            // templateCreationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tempTextBox);
             this.Controls.Add(this.tempLabel);
-            this.Name = "Form1";
+            this.Name = "templateCreationForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
