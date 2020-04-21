fc17c577c53986edcb550e26c00857655d3f5731
vince-amela <vincent.amela@pcc.edu>
2020-04-20 21:11:16 -0700

    more documentation

--- FINS/TemplateCreation/templateCreationForm.cs
+++ FINS/TemplateCreation/templateCreationForm.cs
@@ -9,6 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+/*
+Author: Vince Amela
+Date: 4/19/20
+Class: CIS 234A
+Assignment: 3
+Bugs: 
+      *  none at this time 
+*/
+
+
 namespace TemplateCreation
 {
     public partial class templateCreationForm : Form
