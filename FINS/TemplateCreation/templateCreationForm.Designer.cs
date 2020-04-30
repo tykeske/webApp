87168d218473b67f5cc81b17aa821b7c656f4fe3
vince-amela <vincent.amela@pcc.edu>
2020-04-29 17:57:15 -0700

    ui change

--- FINS/TemplateCreation/templateCreationForm.Designer.cs
+++ FINS/TemplateCreation/templateCreationForm.Designer.cs
@@ -447,7 +447,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 821);
+            this.ClientSize = new System.Drawing.Size(893, 842);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.templatesGroupBox);
