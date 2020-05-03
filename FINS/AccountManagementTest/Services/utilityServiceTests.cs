596e647236a95e3e9d55594e33019d08f39bd490
Nick Dechativong <nick.dechativong@pcc.edu>
2020-05-03 08:40:38 -0700

    Added initial unit tests for AccountManagement

---
+++ FINS/AccountManagementTest/Services/utilityServiceTests.cs
@@ -0,0 +1,49 @@
+﻿/*
+    Author:         Nick Dechativong
+    Created Date:   05/01/2020
+    Class:          CIS 234A
+    Objective:      Unit testing for the utilityService class 
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AccountManagement.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountManagement.Services.Tests
+{
+    [TestClass()]
+    public class utilityServiceTests
+    {
+        [TestMethod()]
+        [Timeout(2000)]  // Milliseconds
+        public void passwordHasher_NotEqual()
+        {
+            // arrange
+            string passWord = "TeamApex123$!";
+            var util = new utilityService();
+
+            // act
+            String passWordHash = util.passwordHasher(passWord);
+
+            // assert
+            Assert.AreNotEqual(passWord, passWordHash);
+        }
+
+        [TestMethod()]
+        [Timeout(2000)]  // Milliseconds
+        public void passwordHasher_NotEmpty()
+        {
+            // arrange
+            string passWord = "TeamApex123$!";
+            var util = new utilityService();
+
+            // act
+            String passWordHash = util.passwordHasher(passWord);
+
+            // assert
+            Assert.AreNotEqual("", passWordHash);
+        }
+    }
+}
