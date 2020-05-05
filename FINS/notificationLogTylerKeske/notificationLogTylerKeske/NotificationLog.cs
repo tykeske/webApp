324e7810dafd0b10d81f772bd342297ae5b17e70
TylerTeamApex <tyler.keske@pcc.edu>
2020-05-04 23:47:08 -0700

    fixed minor error

--- FINS/notificationLogTylerKeske/notificationLogTylerKeske/NotificationLog.cs
+++ FINS/notificationLogTylerKeske/notificationLogTylerKeske/NotificationLog.cs
@@ -19,7 +19,7 @@ namespace notificationLogTylerKeske
 {
     public partial class NotificationLog : Form
     {
-        public NotificationLog(int userID)
+        public NotificationLog()
         {
             InitializeComponent();
         }
